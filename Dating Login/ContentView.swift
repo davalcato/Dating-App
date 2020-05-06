6931fe85cf095265154f422480156875b14ce1c6
davalcato <davalcato@yahoo.com>
2020-05-05 19:11:45 -0700

    first commit

---
+++ Dating Login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dating Login
+//
+//  Created by Daval Cato on 5/5/20.
+//  Copyright © 2020 Daval Cato. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
